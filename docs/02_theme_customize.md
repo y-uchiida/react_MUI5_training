e3593a5a244097990fbf383bc947f4d33980a2e6
yugo uchiida <y.uchiida@nirneege.com>
2022-10-28 16:45:34 +0900

    change font size by customizing default theme

---
+++ docs/02_theme_customize.md
@@ -0,0 +1,85 @@
+# テーマのカスタマイズ
+
+MUI がデフォルトで Material UI の見栄えを実装したテーマを提供しているが、それを上書きや拡張をして利用したい場合がある  
+その場合は、createTheme でテーマを作成する  
+差分のみ記述すればよいので、記述量は多くない
+
+## MUI コンポーネントのバリアントを修正する
+
+```tsx
+/* このコンポーネントに適用するテーマを作成する
+ * MUIが用意しているデフォルトのテーマを読み込んでいるので、
+ * それを上書きする形で記述できる
+ */
+const theme = createTheme({
+  /* コンポーネントのスタイル設定を上書きする */
+  components: {
+    /* MUI に含まれる内容はMuiXXXXXの形式で名称を指定する */
+    MuiTypography: {
+      variants: [
+        {
+          /* Typography コンポーネントで body2 バリアントを指定した際のfont-size を11に上書きする */
+          props: { variant: "body2" },
+          style: { fontSize: 11 },
+        },
+      ],
+    },
+  },
+});
+
+export const SampleComponent = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography variant="body2">font-size 11</Typography>
+    </ThemeProvider>
+  );
+};
+```
+
+## MUI コンポーネントに新しいバリアントを追加する
+
+TypeScript を使っている場合、定義されていないバリアント名を createTheme で利用するとエラーになる  
+あらかじめ、モジュール拡張でバリアント名を追加しておく
+
+```tsx
+/* Typography コンポーネントを読み込みしておく */
+import { Typography } from "@mui/material";
+
+/* Typography コンポーネントでbody3 バリアントを利用するため、
+ * モジュール拡張でバリアントを追加しておく
+ */
+declare module "@mui/material/Typography" {
+  /* interface は、 <MUIコンポーネント名>PropsVariantOverrides */
+  interface TypographyPropsVariantOverrides {
+    body3: true;
+  }
+}
+
+/* このコンポーネントに適用するテーマを作成する
+ * MUIが用意しているデフォルトのテーマを読み込んでいるので、
+ * それを上書きする形で記述できる
+ */
+const theme = createTheme({
+  /* コンポーネントのスタイル設定を上書きする */
+  components: {
+    /* MUI に含まれる内容はMuiXXXXXの形式で名称を指定する */
+    MuiTypography: {
+      variants: [
+        {
+          /* Typography コンポーネントで body3 バリアントを指定した際のfont-size を9に設定する */
+          props: { variant: "body3" },
+          style: { fontSize: 9 },
+        },
+      ],
+    },
+  },
+});
+
+export const SampleComponent = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography variant="body2">font-size 11</Typography>
+    </ThemeProvider>
+  );
+};
+```
--- src/components/molecules/TourCard.tsx
+++ src/components/molecules/TourCard.tsx
@@ -1,46 +1,82 @@
 import React from 'react'
-import { Box, Container, Grid, Paper, Rating, Typography } from '@mui/material'
+import { Box, createTheme, Paper, Rating, ThemeProvider, Typography } from '@mui/material'
 import { AccessTime } from '@mui/icons-material'
+
+/* Typography コンポーネントでbody3 バリアントを利用するため、
+ * モジュール拡張でバリアントを追加しておく
+ */
+declare module '@mui/material/Typography' {
+	/* interface は、 <MUIコンポーネント名>PropsVariantOverrides */
+	interface TypographyPropsVariantOverrides {
+		body3: true
+	}
+}
+
+/* このコンポーネントに適用するテーマを作成する
+ * MUIが用意しているデフォルトのテーマを読み込んでいるので、
+ * それを上書きする形で記述できる
+ */
+const theme = createTheme({
+	/* コンポーネントのスタイル設定を上書きする */
+	components: {
+		/* MUI に含まれる内容はMuiXXXXXの形式で名称を指定する */
+		MuiTypography: {
+			variants: [
+				{
+					/* Typography コンポーネントで body2 バリアントを指定した際のfont-size を11に上書きする */
+					props: { variant: 'body2' },
+					style: { fontSize: 11 }
+				},
+				{
+					/* Typography コンポーネントで body3 バリアントを指定した際のfont-size を9に上書きする */
+					props: { variant: 'body3' },
+					style: { fontSize: 9 }
+				},
+			]
+		}
+	}
+});
 
 const TourCard = () => {
 	return (
 		<>
-			<Paper elevation={3}>
+			<ThemeProvider theme={theme}>
+				<Paper elevation={3}>
+					<Box component="img"
+						sx={{ width: "100%", height: "5rem" }}
+						src="https://images.unsplash.com/photo-1463695970743-ae65cca05743?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+						alt="" />
 
-				<Box component="img"
-					sx={{ width: "100%", height: "5rem" }}
-					src="https://images.unsplash.com/photo-1463695970743-ae65cca05743?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-					alt="" />
-
-				<Box paddingX={1}>
-					<Typography variant="subtitle1" component="h2">
-						Immerse into the Falls
-					</Typography>
-
-					<Box sx={{ display: "flex", alignItems: "center" }}>
-						<AccessTime sx={{ width: 12.5 }} />
-						<Typography variant='body2' component='p' marginLeft={0.5}>
-							5 hours
+					<Box paddingX={1}>
+						<Typography variant="subtitle1" component="h2">
+							Immerse into the Falls
 						</Typography>
+
+						<Box sx={{ display: "flex", alignItems: "center" }}>
+							<AccessTime sx={{ width: 12.5 }} />
+							<Typography variant='body2' component='p' marginLeft={0.5}>
+								5 hours
+							</Typography>
+						</Box>
+
+						<Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+							<Rating name='read-only' precision={0.5} size="small" value={4.5} readOnly />
+							<Typography variant='body2' component='p' marginLeft={0.5}>
+								4.5
+							</Typography>
+							<Typography variant='body3' component='p' marginLeft={1.5}>
+								(655 reviews)
+							</Typography>
+						</Box>
+
+						<Box>
+							<Typography variant='h6' component='h3' marginLeft={0}>
+								from C $147
+							</Typography>
+						</Box>
 					</Box>
-
-					<Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
-						<Rating name='read-only' precision={0.5} size="small" value={4.5} readOnly />
-						<Typography variant='body2' component='p' marginLeft={0.5}>
-							4.5
-						</Typography>
-						<Typography variant='body2' component='p' marginLeft={1.5}>
-							(655 reviews)
-						</Typography>
-					</Box>
-
-					<Box>
-						<Typography variant='h6' component='h3' marginLeft={0}>
-							from C $147
-						</Typography>
-					</Box>
-				</Box>
-			</Paper>
+				</Paper>
+			</ThemeProvider>
 		</>
 	)
 }
